df8a727b5df6081bea0b58bd27fd63169a2ca0e0
shekharshinde06 <shekharshinde06092000@gmail.com>
2023-09-22 18:58:55 +0530

    assignment 1 que2 sunque 2 uploaded

---
+++ assignment1_que2_2.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+class Date
+{ 
+public:
+  int day;
+  int month;
+  int year;
+
+  void initDate()
+  {
+    day = 1;
+    month = 1;
+    year = 2000;
+  }
+  void acceptDate()
+  {
+    cout << "enter the date in day,month,year=";
+    cin >> day >> month >> year;
+  }
+  void printDate()
+  {
+    cout << "DATE IS DAY=" << day << "MONTH =" << month << "YEAR=" << year << "\n";
+  }
+  bool isleap(int year)
+  {
+    if(year%4==0 && year%100!=0 || year%400==0){
+      return true;
+    }
+    else
+    { 
+      return false;
+    }
+  }
+};
+int main()
+{
+
+   Date d1;
+  int choice;
+  char ch;
+  do
+  {
+    cout << "1.DEFAULT DATE \n 2.TO ENTER DATE \n 3.TO FIND THE YEAR IS LEAP OR NOT \n " << endl;
+    cout << "ENTER YOUR CHOICE=" << endl;
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+      d1.initDate();
+      d1.printDate();
+      break;
+    case 2:
+      d1.acceptDate();
+      d1.printDate();
+      break;
+    case 3:
+         int year;
+         cout<<"ENTER YEAR TO CHECK LEAP OR NOT =";
+         cin>>year;
+         if(d1.isleap(year))
+         {
+          cout<<"YEAR IS LEAP"<<endl;
+         }
+         else
+         {
+          cout<<"YEAR IS NOT YEAR"<<endl;
+         }
+
+         break;
+    default:
+      printf("INVALID CHOICE");
+    }
+    cout << "Do you want to continue? enter 'y' for continue=";
+    cin >> ch;
+  } while(ch == 'y');
+
+  return 0;
+}
